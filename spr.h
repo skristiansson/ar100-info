983223b70b780943ce1d108f8526aa172a2f16e4
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-06-15 09:39:11 +0300

    add include guards to spr.h

--- spr.h
+++ spr.h
@@ -1,3 +1,5 @@
+#ifndef _SPR_H
+#define _SPR_H
 #include "spr-defs.h"
 
 static inline unsigned long mfspr(unsigned long add)
@@ -13,3 +15,4 @@ static inline void mtspr(unsigned long add, unsigned long val)
 {
 	__asm__ __volatile__ ("l.mtspr r0,%1,%0" : : "K" (add), "r" (val));
 }
+#endif
