06412572448357b8904d18ae1bb45eff7513a40c
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-06-15 19:02:43 +0300

    add instruction tests
    Test the presence of optional ORBIS32 class II instructions by
    executing them and setting a flag in the illegal instruction
    exception.
    There's also a test for l.lws, mostly because that instruction
    is missing in older or1200 implementations.

--- main.c
+++ main.c
@@ -30,6 +30,8 @@
 	put_hex(mfspr(spr));	\
 	puts("\n");
 
+extern volatile unsigned int illegal_instruction;
+
 void putchar(char c)
 {
 	uart0_putc(c);
@@ -179,6 +181,275 @@ void test_clk_freq(void)
 	puts(" MHz\n");
 }
 
+static int test_addc(void)
+{
+	volatile unsigned int ra = 1, rb = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.addc %0,%1,%2" : "=r" (rd) : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+static int test_cmov(void)
+{
+	volatile unsigned int ra = 1, rb = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.cmov %0,%1,%2" : "=r" (rd) : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+static int test_cust1(void)
+{
+	illegal_instruction = 0;
+	asm volatile("l.cust1");
+}
+
+static int test_cust2(void)
+{
+	illegal_instruction = 0;
+	asm volatile("l.cust2");
+}
+
+static int test_cust3(void)
+{
+	illegal_instruction = 0;
+	asm volatile("l.cust3");
+}
+
+static int test_cust4(void)
+{
+	illegal_instruction = 0;
+	asm volatile("l.cust4");
+}
+
+static int test_cust5(void)
+{
+	illegal_instruction = 0;
+	asm volatile("l.cust5");
+}
+
+static int test_cust6(void)
+{
+	illegal_instruction = 0;
+	asm volatile("l.cust6");
+}
+
+static int test_cust7(void)
+{
+	illegal_instruction = 0;
+	asm volatile("l.cust7");
+}
+
+static int test_cust8(void)
+{
+	illegal_instruction = 0;
+	asm volatile("l.cust8");
+}
+
+static int test_div(void)
+{
+	volatile unsigned int ra = 1, rb = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.div %0,%1,%2" : "=r" (rd) : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+static int test_divu(void)
+{
+	volatile unsigned int ra = 1, rb = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.divu %0,%1,%2" : "=r" (rd) : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+static int test_extbs(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.extbs %0,%1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_extbz(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.extbz %0,%1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_exths(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.exths %0,%1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_exthz(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.exthz %0,%1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_extws(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.exths %0,%1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_extwz(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.exthz %0,%1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_ff1(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.ff1 %0,%1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_fl1(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.fl1 %0,%1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_lws(void)
+{
+	volatile unsigned int ra = 0, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.lws %0,0(%1)" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_mac(void)
+{
+	volatile unsigned int ra = 1, rb = 1;
+
+	illegal_instruction = 0;
+	asm volatile("l.mac %0,%1" : : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+static int test_maci(void)
+{
+	volatile unsigned int rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.maci %0,1" : "=r" (rd));
+
+	return !illegal_instruction;
+}
+
+static int test_macrc(void)
+{
+	volatile unsigned int rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.macrc %0" : "=r" (rd));
+
+	return !illegal_instruction;
+}
+
+static int test_macu(void)
+{
+	volatile unsigned int ra = 1, rb = 1;
+
+	illegal_instruction = 0;
+	asm volatile("l.macu %0,%1" : : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+
+static int test_mul(void)
+{
+	volatile unsigned int ra = 1, rb = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.mul %0,%1,%2" : "=r" (rd) : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+static int test_muli(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.muli %0,%1,1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
+static int test_mulu(void)
+{
+	volatile unsigned int ra = 1, rb = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.mulu %0,%1,%2" : "=r" (rd) : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+static int test_ror(void)
+{
+	volatile unsigned int ra = 1, rb = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.ror %0,%1,%2" : "=r" (rd) : "r" (ra), "r" (rb));
+
+	return !illegal_instruction;
+}
+
+static int test_rori(void)
+{
+	volatile unsigned int ra = 1, rd;
+
+	illegal_instruction = 0;
+	asm volatile("l.rori %0,%1,1" : "=r" (rd) : "r" (ra));
+
+	return !illegal_instruction;
+}
+
 int main(void)
 {
 	unsigned int spr;
@@ -187,6 +458,69 @@ int main(void)
 	uart0_init();
 
 	puts("\n");
+
+	puts("Test support for l.addc...");
+	test_addc() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cmov...");
+	test_cmov() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cust1...");
+	test_cust1() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cust2...");
+	test_cust2() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cust3...");
+	test_cust3() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cust4...");
+	test_cust4() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cust5...");
+	test_cust5() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cust6...");
+	test_cust6() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cust7...");
+	test_cust7() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.cust8...");
+	test_cust8() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.div...");
+	test_div() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.divu...");
+	test_divu() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.extbs...");
+	test_extbs() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.extbz...");
+	test_extbz() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.exths...");
+	test_exths() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.exthz...");
+	test_exthz() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.extws...");
+	test_extws() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.extwz...");
+	test_extwz() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.ff1...");
+	test_ff1() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.fl1...");
+	test_fl1() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.lws...");
+	test_lws() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.mac...");
+	test_mac() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.maci...");
+	test_maci() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.macrc...");
+	test_macrc() ? puts("yes\n") : puts("no\n");
+/*
+	puts("Test support for l.macu...");
+	test_macu() ? puts("yes\n") : puts("no\n");
+*/
+	puts("Test support for l.mul...");
+	test_mul() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.muli...");
+	test_muli() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.mulu...");
+	test_mulu() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.ror...");
+	test_ror() ? puts("yes\n") : puts("no\n");
+	puts("Test support for l.rori...");
+	test_rori() ? puts("yes\n") : puts("no\n");
 
 	puts("Init DRAM...");
 	mctl_init();
